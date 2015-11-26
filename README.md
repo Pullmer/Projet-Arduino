eac61ba3f80754a145ee251300939fe1faa1a49f
Bruce <cyprienthomas@gmail.com>
2015-11-26 18:55:05 +0100

    ajout de code python

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Projet-Arduino
 Projet Long - Drone roulant Arduino
 
+Version Python que j'utilise : 3.2
+
+Pensez à garder tout les fichiers encodés en UTF-8
+
+Tutoriel que j'utilise pour Tkinter (bibliothèque pour GUI) : http://zetcode.com/gui/tkinter/
---
+++ main.py
@@ -0,0 +1,20 @@
+"""       Projet Arduino        """
+"""     script de lancement     """
+
+from tkinter import Tk, Frame, BOTH
+
+from mainWindowClass import mainWindow
+
+def main():
+  
+    root = Tk()
+    root.geometry("250x150+300+300")
+    app = mainWindow(root)
+    root.mainloop()  
+
+
+""" éxecute la fonction main (seulement si on lance le script main.py et pas si on ne fait que l'importer à partir d'un autre fichier) """
+	
+if __name__ == '__main__':
+    main()  
+
