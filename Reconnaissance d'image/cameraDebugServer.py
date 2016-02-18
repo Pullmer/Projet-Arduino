e72d211d10ff083c584aa17919f7189b2b87ce9a
Jonas1312 <jonas.maison@outlook.com>
2016-02-18 22:20:08 +0100

    Amélioration du serveur de streaming camera

--- Reconnaissance d'image/cameraDebugServer.py
+++ Reconnaissance d'image/cameraDebugServer.py
@@ -11,30 +11,35 @@ import socket
 ########################################################################
 class CameraDebugServer:
     """"""
-    
+
     #----------------------------------------------------------------------
     def __init__(self, port = 8123):
         """Constructor"""
         self.TCP_PORT = port
-        
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.s.bind(('', self.TCP_PORT))
         self.s.listen(True)
         self.conn, self.addr = self.s.accept()
-        print("socket ouvert")
+        print("Socket ouvert")
 
     #----------------------------------------------------------------------
     def sendData(self, data):
-        """Transmission du flux vidéo sur le réseau"""
-        self.conn.send(str(len(data)).ljust(16))
-        self.conn.send(data)        
+        """Transmission de data sur le réseau"""
+        try:
+            self.conn.send(str(len(data)).ljust(16))
+            self.conn.send(data)
+        except:
+            print("Interruption du socket, en attente...")
+            CameraDebugServer.__init__(self)
 
     #----------------------------------------------------------------------
     def __del__(self):
         """Lorsque la classe se ferme on close le socket"""
-        print("socket closed")
-        self.s.close()
-
+        try:
+            self.s.close()
+            print("Socket closed")
+        except:
+            pass
 
 
