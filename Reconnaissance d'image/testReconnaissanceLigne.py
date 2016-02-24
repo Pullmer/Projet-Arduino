8a0cd909b161a63cde910cb88814108b86949f08
Jonas1312 <jonas.maison@outlook.com>
2016-02-24 19:21:13 +0100

    Update suivi ligne
    Ajout d'une fonction pour supprimer les lignes quasi-superposées

--- Reconnaissance d'image/testReconnaissanceLigne.py
+++ Reconnaissance d'image/testReconnaissanceLigne.py
@@ -2,7 +2,7 @@
 #coding:utf-8
 """
   Author:  Jonas
-  Purpose: Test reconnaissance de ligne
+  Purpose: Test unitaire reconnaissance de ligne
   Created: 16/02/2016
 """
 
@@ -24,22 +24,40 @@ def recvall(sock, count):
 
 #----------------------------------------------------------------------
 def nothing(x):
-    """Do nothing"""
+    """Magic function : Do nothing"""
     pass
+
+#----------------------------------------------------------------------
+def deleteSameLines(lines):
+    """Permet de supprimer les lignes quasi-superposées"""
+    index = list() # Liste qui contiendra les index des coordonnées à supprimer
     
+    for i in range(len(lines)):
+        rho_i, theta_i = lines[i][0]
+        for j in range(len(lines)):
+            rho_j, theta_j = lines[j][0]
+            if theta_i != theta_j and rho_i != rho_j and abs(rho_i - rho_j) < 5 and abs(theta_i - theta_j) < 0.2:
+                index.append(j)
+                
+    index = sorted(index[:len(index)/2], reverse=True) # Supprime les paires d'index et trie la liste
+    for i in index:
+        lines = numpy.delete(lines, i, 0)
+        
+    return lines
+
 #----------------------------------------------------------------------
 
-resolution = (420, 340) # Résolution caméra
-TCP_IP = '10.0.0.200' # Adresse du serveur
+resolution = (640, 480) # Résolution caméra
+TCP_IP = '10.12.152.110' # Adresse du serveur
 TCP_PORT = 8123 # Port de communication
 
 sock = socket.socket()
 sock.connect((TCP_IP, TCP_PORT))
 
 cv2.namedWindow('Reglages')
-cv2.createTrackbar("t1", "Reglages", 47, 255, nothing)
-cv2.createTrackbar("t2", "Reglages", 29, 255, nothing)
-cv2.createTrackbar("Threshold", "Reglages", 30, 255, nothing) # Longueur minimale admissible d'un segment
+cv2.createTrackbar("t1", "Reglages", 132, 255, nothing)
+cv2.createTrackbar("t2", "Reglages", 114, 255, nothing)
+cv2.createTrackbar("Threshold", "Reglages", 100, 255, nothing) # Longueur minimale admissible d'un segment
 
 while(True):
 
@@ -47,7 +65,7 @@ while(True):
     
     if(length):
         stringData = recvall(sock, int(length))
-        data = numpy.fromstring(stringData, dtype='uint8').reshape(resolution[1], resolution[0], -1)    
+        data = numpy.fromstring(stringData, dtype='uint8').reshape(resolution[1], resolution[0], -1) # Image reçue originale  
         
         #----------------------------------------------------------------------
         
@@ -57,11 +75,12 @@ while(True):
         
         imgGray = cv2.cvtColor(data, cv2.COLOR_BGR2GRAY) # Conversion en niveau de gris
         imgEdges = cv2.Canny(imgGray, t1, t2) # Détection des bords
-        HoughLines = cv2.HoughLines(imgEdges, 1, numpy.pi/180, HoughLinesThreshold)
+        HoughLines = cv2.HoughLines(imgEdges, 1, numpy.pi/180, HoughLinesThreshold) # Transformée de Hough Lines
         imgHoughLines = data.copy()
+        HoughLines = deleteSameLines(HoughLines)
         
         try:
-            for i in range(min(20, len(HoughLines))):
+            for i in range(min(30, len(HoughLines))):
                 for rho, theta in HoughLines[i]:
                     a, b = numpy.cos(theta), numpy.sin(theta)
                     x0, y0 = a*rho, b*rho
@@ -70,11 +89,11 @@ while(True):
                     x2 = int(x0 - 1000*(-b))
                     y2 = int(y0 - 1000*(a))
                 
-                    cv2.line(imgHoughLines, (x1, y1), (x2, y2), (0, 0, 255), 2)       
+                    cv2.line(imgHoughLines, (x1, y1), (x2, y2), (0, 0, 255), 2) # Ajout des lignes détectées sur l'image originale      
         except:
             pass
         
-        cv2.imshow("imgGray", imgGray); 
+        #cv2.imshow("imgGray", imgGray); 
         cv2.imshow("imgEdges", imgEdges); 
         cv2.imshow("Houghlines", imgHoughLines);  
         
