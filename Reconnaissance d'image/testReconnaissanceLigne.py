95eb0644fc3019a528b975dd9604a5746c321322
Jonas1312 <jonas.maison@outlook.com>
2016-02-27 16:11:31 +0100

    up

--- Reconnaissance d'image/testReconnaissanceLigne.py
+++ Reconnaissance d'image/testReconnaissanceLigne.py
@@ -10,6 +10,10 @@ import socket
 import numpy
 import cv2
 
+resolution = (640, 480) #Résolution caméra
+TCP_IP = '10.12.152.110' #Adresse du serveur
+TCP_PORT = 8123 #Port de communication
+
 #----------------------------------------------------------------------
 def recvall(sock, count):
     """Receive data"""
@@ -25,17 +29,9 @@ def recvall(sock, count):
 
 #----------------------------------------------------------------------
 def nothing(x):
-    """Magic function : do nothing !"""
+    """Magic function : do nothing"""
     pass
 
-#----------------------------------------------------------------------
-def deleteSameLines(lines):
-    """Permet de supprimer les lignes quasi-confondues"""
-    if lines is not None:
-        pass
-        
-    return lines
-        
 #----------------------------------------------------------------------
 def rhothetaToPoints(rho, theta):
     """Conversion de rho et theta en deux points constituant une droite"""
@@ -46,30 +42,49 @@ def rhothetaToPoints(rho, theta):
     x2 = int(x0 - 1000*(-b))
     y2 = int(y0 - 1000*(a))
     
+    if y1 > y2:
+        (x1, y1), (x2, y2) = (x2, y2), (x1, y1)
     return (x1, y1), (x2, y2)
 
 #----------------------------------------------------------------------
 def lineToAngle(((x1, y1), (x2, y2))):
     """Calcul l'angle d'une droite par rapport à la verticale"""
-    if x1 != x2:
-        return numpy.arctan((y1 - y2)/(x1 - x2))*180/numpy.pi
+    if y1 != y2:
+        return numpy.arctan(float(x1 - x2)/(y1 - y2))*180/numpy.pi
     else:
-        return 0
+        return 90.0
+
 #----------------------------------------------------------------------
-def drawMiddleLine(img, lines):
-    """Trace la ligne à suivre"""
+def keepVerticalLines(lines):
+    """Garde uniquement les lignes verticales"""
     if lines is not None:
         index = list()
         for i in range(len(lines)):
             for rho, theta in lines[i]:
-                if not (theta > numpy.pi/180*155 or theta < numpy.pi/180*10): #Pour une ligne horizontale
+                if not (theta > numpy.pi/180*130 or theta < numpy.pi/180*40):
                     index.append(i)
     
         #Suppression des lignes horizontales
         for i in sorted(index, reverse=True):
             lines = numpy.delete(lines, i, 0)
+     
+    return lines
+
+#----------------------------------------------------------------------
+def deleteSameLines(lines):
+    """Supprime les lignes quasi-superposées"""
+    pass
+
+    return lines
+    
+#----------------------------------------------------------------------
+def drawMiddleLine(img, lines):
+    """Trace la ligne qui doit être suivie par le robot"""
+    if lines is not None:
         
-        a = len(lines)
+        #On supprime les lignes horizontales
+        lines = keepVerticalLines(lines)
+        
         if len(lines) >= 2:
             (x1a, y1a), (x2a, y2a) = rhothetaToPoints(lines.item(0), lines.item(1))
             (x1b, y1b), (x2b, y2b) = rhothetaToPoints(lines.item(2), lines.item(3))    
@@ -78,7 +93,8 @@ def drawMiddleLine(img, lines):
             y1 = (y1a + y1b)/2
             y2 = (y2a + y2b)/2
             cv2.line(img, (x1, y1), (x2, y2), (0, 255, 255), 7)
-    
+            print("Deviation : {0}".format(lineToAngle(((x1,y1),(x2,y2)))))
+            
     return drawLines(img, lines, color=(0, 255, 0))
     
 #----------------------------------------------------------------------
@@ -96,10 +112,6 @@ def drawLines(img, lines, color=(0, 0, 255), thickness=2):
 #----------------------------------------------------------------------
 def main():
     """Fonction main"""
-    resolution = (640, 480) #Résolution caméra
-    TCP_IP = '10.12.152.110' #Adresse du serveur
-    TCP_PORT = 8123 #Port de communication
-    
     sock = socket.socket()
     sock.connect((TCP_IP, TCP_PORT))
     print("Socket ouvert")
@@ -110,7 +122,6 @@ def main():
     cv2.createTrackbar("Threshold", "Reglages", 65, 255, nothing) #Longueur minimale admissible d'un segment
     
     while(True):
-    
         length = recvall(sock, 16)
         
         if(length):
